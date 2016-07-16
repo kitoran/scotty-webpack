14294923108e8c29564d553830b586ccf3fe0d02
тn <kitttoran@gmail.com>
2016-07-16 04:17:29 +0300

    another initial commit

---
+++ main.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE NoMonomorphismRestriction
+           , OverloadedStrings
+           , DeriveGeneric
+           , ImplicitParams #-}
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.ByteString.Lazy
+import Data.Text.Lazy.Encoding
+import GHC.Generics
+
+import Web.Scotty
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.FromRow
+import Data.Aeson
+import System.Posix.Syslog
+
+
+main = withSyslog defaultConfig (\log' -> let ?log = log' in server)
+
+server :: (?log::SyslogFn) => IO ()
+server = do
+  scotty 3000 $ do
+    get "/:string" $ do
+      string <- param "string"
+      a <- liftIO $ queryPhoneDatabase string
+      Web.Scotty.json a    
+
+queryPhoneDatabase :: (?log::SyslogFn) => String -> IO [PhonebookRecord]
+queryPhoneDatabase string = do 
+    conn <- connectPostgreSQL ""
+    i <- query conn "select string, number from helloworldt where string = ?;" $ Only string
+    when (Prelude.null i)
+         (?log USER Warning "No such record in database")
+    return i
+    
+data PhonebookRecord = PhonebookRecord { phonebookName :: String, phonebookNumber :: Int } deriving (Show, Eq, Generic)
+
+instance ToJSON PhonebookRecord where
+
+instance FromRow PhonebookRecord where
+    fromRow = PhonebookRecord <$> field <*> field
